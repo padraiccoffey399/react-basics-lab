598816409f352d606bb0bf240f11e9fd9ac5109e
padraiccoffey399 <padraiccoffey@gmail.com>
2026-09-22 11:26:01 +0100

    Added task state

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -1,19 +1,27 @@
 import './App.css';
 import Task from './components/Task';
+import React, { useState } from 'react';
+
 
 function App() {
-  return (
+    const [ taskState, setTaskState ] = useState({
+    tasks: [
+      { title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { title: "Tidy up", deadline: "Today" }
+    ]
+  });
+
+
+    return (
     <div className="container">
       <h1>Tasky</h1>
-      <Task title="Dishes " deadline = "today" description="Clean ad put away Dishes" />
-
-      <Task title="laundry " deadline = "in 1 hour" description="fold laundry and put away" />
-       
-      <Task title="dinner " deadline = "tomorrow" description="Prepare and cook dinner" />
-       
-
+      <Task title={taskState.tasks[0].title} deadline={taskState.tasks[0].deadline} description={taskState.tasks[0].description} />
+      <Task title={taskState.tasks[1].title} deadline={taskState.tasks[1].deadline} description={taskState.tasks[1].description} />
+      <Task title={taskState.tasks[2].title} deadline={taskState.tasks[2].deadline} description={taskState.tasks[2].description} />
     </div>
   );
+
 }
 
 export default App;
